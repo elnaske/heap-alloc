f916311e3bea982b1c0c1ad2e8062b6a64838b60
elnaske <naske.elias@outlook.com>
2026-06-14 10:35:00 +0200

    Add heap_init()

--- halloc.c
+++ halloc.c
@@ -1,27 +1,61 @@
-#include<stdio.h>
-#include<stdint.h>
-#include<sys/mman.h>
-#include<unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 enum HEAP_STATUS {
     HEAP_OK,
     HEAP_ERR_MMAP,
 };
 
-void *heap_alloc(size_t size);
+typedef struct HeapChunk {
+    uint32_t size;
+    bool free;
+    struct HeapChunk *next;
+    struct HeapChunk *prev;
+} HeapChunk;
+
+typedef struct {
+    HeapChunk *start;
+    uint32_t avail;
+} HeapData;
+
+
+int heap_init(HeapData *heap) {
+    int heap_size = getpagesize();
+    void *heap_start = mmap(NULL, heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    
+    if (heap_start == (void *)-1) {
+        return HEAP_ERR_MMAP;
+    }
+
+    heap->start = heap_start;
+    heap->avail = (uint32_t)heap_size - sizeof(HeapChunk);
+    
+    HeapChunk chunk = {
+        .size = heap->avail,
+        .free = true,
+        .prev = NULL,
+        .next = NULL,
+    };
+
+    *(heap->start) = chunk;
+
+    return HEAP_OK;
+}
+
+void *heap_alloc(uint32_t size);
 
 void heap_free(void *ptr);
 
 int main() {
-    int heap_size = getpagesize();
-    void *heap_start = mmap(NULL, heap_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    HeapData heap;
+    heap_init(&heap);
 
-    if (!heap_start) {
-        return HEAP_ERR_MMAP;
-    }
-
-    printf("Heap start: %p\n", heap_start);
-    printf("Heap size: %d\n", heap_size);
+    printf("Heap start: %p\n", (void *)heap.start);
+    printf("Available memory: %d\n", heap.avail);
+    printf("First chunk->size: %d\n", heap.start->size);
 
     return HEAP_OK;
 }
